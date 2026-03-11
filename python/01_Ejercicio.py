aa99af4b20c171bb29ade69d5b085ef31bcdeeba
Valentin <valentingeggel@gmail.com>
2026-03-11 14:33:27 -0300

    Ejercicio Suma en py

---
+++ python/01_Ejercicio.py
@@ -0,0 +1,9 @@
+#1-Dados 3 números, sumarlos. Solo sabe sumar de a dos. 
+
+n1= int(input("Ingresa el primer número: "))
+n2= int(input("Ingresa el segundo número: "))
+n3= int(input("Ingresa el tercer número: "))
+
+x=(n1+n2)+n3
+
+print("La suma de los números seleccionados es:", x)
