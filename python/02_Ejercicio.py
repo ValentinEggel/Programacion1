f2fd869fa0a1a87f07e66f0aaa8d8b3f6f664622
Valentin <valentingeggel@gmail.com>
2026-03-11 14:41:07 -0300

    Ejercicio perímetro triángulo

---
+++ python/02_Ejercicio.py
@@ -0,0 +1,9 @@
+#2-Dados 3 lados de un triángulo, hallar el perímetro 
+
+l1= float(input("Ingresa el largo del primer lado: "))
+l2= float(input("Ingresa el largo del segundo lado: "))
+l3= float(input("Ingresa el largo del tercer lado: "))
+
+p=l1+l2+l3
+
+print("El valor del perímetro del triángulo ingresado es de: ", p , " cm.")
