a20ade2e5cd7fd63149cc8579d483863cca89585
Valentin <valentingeggel@gmail.com>
2026-03-13 16:58:23 -0300

    Pruebas 02 y 03

---
+++ python/Prueba02.py
@@ -0,0 +1,9 @@
+nombre = "Brian"
+edad = 25
+ciudad = "Rafaela"
+estudiante = True
+
+print("Hola, me llamo", nombre)
+print("Tengo", edad, "años")
+print("Vivo en", ciudad)
+print(estudiante)
---
+++ python/Prueba03.py
@@ -0,0 +1,9 @@
+pelicula = input("Ingresá tu película favorita: ")
+año = input("Ingresá el año de salida: ")
+vistas = input("¿La viste más de una vez? (True/False): ")
+puntuacion = input("Ingresá tu puntuación personal: ")
+
+print("Película:", pelicula)
+print("Año:", año)
+print("¿La vi más de una vez?", vistas)
+print("Mi puntuación:", puntuacion)
